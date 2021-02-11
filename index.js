9705f512b08b93546e41bd0aebfe7d672af355c7
Simeon Widdis <sawiddis@gmail.com>
2021-02-10 17:24:34 -0700

    Environment setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const routes = require("./routes/routes");
+
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", routes.index);
+
+app.listen(3000);
