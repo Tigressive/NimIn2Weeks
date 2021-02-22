b7d81aa7860d6c37da59b6cdacc5416102f7e047
SawyerBenson9090 <SawyerBenson9090@gmail.com>
2021-02-22 14:11:17 -0700

    sign up post to mongo

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const routes = require("./routes/routes");
+const dataRoutes = require('./routes/dataRoutes');
 
 const app = express();
-
+app.use(bodyParser.json())
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", routes.index);
+app.post('/signup', dataRoutes.signupPost);
 
 app.listen(3001);
