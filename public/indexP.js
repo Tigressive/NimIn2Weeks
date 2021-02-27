077fdb0382e19b960d5aaba2759638951b97ea88
Simeon Widdis <sawiddis@gmail.com>
2021-02-26 17:39:03 -0700

    Implement game on nim page without input/display

--- public/indexP.js
+++ public/indexP.js
@@ -1,14 +1,27 @@
-const menuAbout = document.getElementById('menuAbout');
+const menuAbout = document.getElementById("menuAbout");
 
-const startBtn = document.getElementById('btnStart');
+const startBtn = document.getElementById("btnStart");
 
 // const handleAboutClick = evt => {
 //     document.location.href = 'about.html';
 // }
 
-const handleStartClick = evt => {
-    document.location.href = 'nim.html';
-}
+const handleStartClick = (evt) => {
+  let difficulties = document.getElementsByName("difficulty");
+  let difficulty;
+
+  for (diff of difficulties) {
+    if (diff.checked) {
+      difficulty = diff.value;
+    }
+  }
+
+  let misere = document.getElementById("misere").checked;
+
+  // TODO: Send difficulty to nim on other side.
+
+  document.location.href = "nim.html";
+};
 
 // menuAbout.addEventListener("click", handleAboutClick);
 
