c3813d26af8a5eda4bda6d2197ffb6ab7f11f48a
SawyerBenson9090 <SawyerBenson9090@gmail.com>
2021-03-03 17:41:06 -0700

    fixing my github errors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const routes = require("./routes/routes");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", routes.index);
 app.get('/about', routes.about);
+app.post('/signup', bodyParser, routes.signupPost);
+//app.post('/login', routes.loginPost);
 
 app.listen(3001);
