118536a4adc99ee8259f4397fb6538001f0444fc
SawyerBenson9090 <SawyerBenson9090@gmail.com>
2021-03-05 11:51:44 -0700

    added backend functionality

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const routes = require("./routes/routes");
 const bodyParser = require("body-parser");
+const expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+let urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+app.use(function(req,res,next){
+    res.locals.session = req.session;
+    next();
+});
+
+app.use(cookieParser('whatever'));
+
+app.use(expressSession({
+  secret: 'whatever',
+  saveUninitialized: true,
+  resave:true
+}));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
@@ -10,7 +29,9 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", routes.index);
 app.get('/about', routes.about);
-app.post('/signup', routes.signupPost);
-//app.post('/login', routes.loginPost);
+app.post('/signup', urlencodedParser, routes.signupPost);
+app.post('/login', urlencodedParser, routes.loginPost);
+app.get('/logout',routes.logout);
+app.patch('/updateGamesWon', routes.updateGamesWon)
 
 app.listen(3001);
--- routes/routes.js
+++ routes/routes.js
@@ -19,7 +19,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: String,
     password: String,
-    email: String
+    email: String,
+    gamesWon: Number
 }, {collection: "Users"});
 
 const User = mongoose.model(userCollectionName, UserSchema);
@@ -34,12 +35,66 @@ exports.signupPost = (req, res) => {
         var signupInput = {
             username: body.username, 
             password: hash,
-            email: body.email
+            email: body.email,
+            gamesWon: 0
         };
-        console.log("hello");
+        
         addUserToDb(signupInput);
     });
 };
+
+
+exports.loginPost = async (req, res) => {
+    var body = req.body;
+    console.log(body);
+    var user = await User.findOne({ username: body.username });
+    var valid = false;
+    if (user) {
+        valid = await bcrypt.compare(req.body.password, user.password);
+    }
+    if(user && valid)
+    {
+        req.session.user = {
+            isAuthenticated:true,
+            username: user.username,
+            gamesWon: user.gamesWon
+        }
+
+        console.log(req.session.user.username)
+      
+        //res.redirect("/");
+    } else {
+        var model = {
+            message: "Username or password are invalid!",
+            "title": "Login",
+            "nav":nav
+        };
+
+      
+    }
+};
+exports.updateGamesWon = (req, res) => {
+    console.log("hello");
+    User.find({"username": req.session.user.username}, (err, user) => {
+        if (err) return console.error(err);
+        user[0].gamesWon = req.body.gamesWon;
+        console.log(user);
+        user[0].save((err, user) => {
+            if (err) return console.error(err);
+            console.log(req.session.user.username + ' updated');
+        });
+    });
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy(err => {
+      if(err){
+        console.log(err);
+      } else {
+        res.sendfile("./public/index.html")
+      }
+    })
+  }
 
 async function addUserToDb(user) {
     console.log("poggers");
@@ -47,8 +102,9 @@ async function addUserToDb(user) {
     await newUser.save();
 }
 
+
 exports.index = (req, res) => {
-  res.render("index");
+    res.sendfile("./public/index.html");
 };
 
 exports.about = (req, res) => {
