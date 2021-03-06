61c2db3dd394d7ad2f960e27d32668d6a5936c57
Simeon Widdis <sawiddis@gmail.com>
2021-03-05 17:11:42 -0700

    Implement option information transferring

--- public/indexP.js
+++ public/indexP.js
@@ -18,7 +18,20 @@ const handleStartClick = (evt) => {
 
   let misere = document.getElementById("misere").checked;
 
+  let playStyles = document.getElementsByName("playStyle");
+  let ps;
+  for (playStyle of playStyles) {
+    if (playStyle.checked) {
+      ps = playStyle.value;
+    }
+  }
+
   // TODO: Send difficulty to nim on other side.
+
+  localStorage.setItem('nim_options_difficulty', difficulty);
+  localStorage.setItem('nim_options_misere', misere);
+  localStorage.setItem('nim_options_play_style', ps);
+  
 
   document.location.href = "nim.html";
 };
